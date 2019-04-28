e4319aa30d11bd0d21884940a381feacaaafa19b
Sam Huddart <sam02h.huddart@gmail.com>
2019-04-28 18:34:17 +0100

    Update update.sh

--- update.sh
+++ update.sh
@@ -3,7 +3,7 @@
 version() { echo "$@" | awk -F. '{ printf("%d%03d%03d%03d\n", $1,$2,$3,$4); }'; }
 
 DATAE=`wget -q -O - "https://raw.githubusercontent.com/SamHDev/blumerun/master/version.txt"`
-DATAC=`cat version.txt`
+DATAC=`cat /usr/local/blumerun/version.txt`
 
 if [ $(version $DATAC) -ge $(version $DATAE) ]; then
     echo "Already Up to Date"
