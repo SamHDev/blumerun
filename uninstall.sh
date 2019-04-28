f14b3499c41e64e650effecb3a75d3cdc62e9a09
Sam Huddart <sam02h.huddart@gmail.com>
2019-04-28 15:17:08 +0100

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,2 +1,15 @@
 echo BlumeRun UnInstaller
 echo ----------------------------------------
+echo "Removing Service"
+systemctl stop blumerun2
+systemctl disable blumerun2
+rm /etc/systemd/system/blumerun2.service
+systemctl daemon-reload
+echo "Removing Files"
+rm -R /usr/local/blumerun
+echo "Removing Path"
+sed -i 's#export PATH="/usr/local/blumerun/bin:$PATH"##g' ~/.bashrc
+. ~/.bashrc
+echo Done
+echo ----------------------------------------
+echo "\nBye Bye!\nYou Can Reinstall here:\n    https://github.com/SamHDev/blumerun/"
